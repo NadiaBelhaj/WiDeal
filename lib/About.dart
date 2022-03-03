e6af8526abf0e5d93642250600e0aab2665b38b0
ASEJ1 <79099978+ASEJ1@users.noreply.github.com>
2022-03-03 12:45:37 +0100

    first commit by ahmed

---
+++ lib/About.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class aboutPage extends StatefulWidget {
+  @override
+  _aboutPageState createState() => _aboutPageState();
+}
+
+// ignore: camel_case_types
+class _aboutPageState extends State<aboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("aboutpage"),
+      ),
+    );
+  }
+}
---
+++ lib/events.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class eventsPage extends StatefulWidget {
+  @override
+  _eventsPageState createState() => _eventsPageState();
+}
+
+class _eventsPageState extends State<eventsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("events page"),
+      ),
+    );
+  }
+}
